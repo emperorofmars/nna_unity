5dec1cd11174a6ce3ba444901669d91c78574abc
emperorofmars <mars@squirrel.pub>
2024-12-17 20:55:31 +0100

    humanoid limits name serializer fixup

--- NNA/Runtime/UnityToNNAUtils/Exporters/NNA_HumanoidLimits_Serializer.cs
+++ NNA/Runtime/UnityToNNAUtils/Exporters/NNA_HumanoidLimits_Serializer.cs
@@ -21,25 +21,17 @@ namespace nna.UnityToNNAUtils
 				{
 					if(bone.limit.useDefaultValues == false)
 					{
-						Debug.Log(bone.humanName);
-						Debug.Log(bone.limit.useDefaultValues);
-						Debug.Log(bone.limit.min);
-						Debug.Log(bone.limit.max);
-
 						var nameDefinition = "$HuLim";
 
 						if(bone.limit.min.x != 0)
-						{
 							nameDefinition += "T" + (float)System.Math.Round(bone.limit.min.x, 2) + "," + (float)System.Math.Round(bone.limit.max.x, 2);
-						}
 						if(bone.limit.min.y != 0)
-						{
 							nameDefinition += "S" + (float)System.Math.Round(bone.limit.min.y, 2) + "," + (float)System.Math.Round(bone.limit.max.y, 2);
-						}
 						if(bone.limit.min.z != 0)
-						{
 							nameDefinition += "P" + (float)System.Math.Round(bone.limit.min.z, 2) + "," + (float)System.Math.Round(bone.limit.max.z, 2);
-						}
+
+						if(bone.limit.axisLength > 0.00001)
+							nameDefinition += "BL" + (float)System.Math.Round(bone.limit.axisLength, 3);
 
 						ret.Add(new SerializerResult(){
 							NNAType = NNA_Twist_JsonProcessor._Type,
